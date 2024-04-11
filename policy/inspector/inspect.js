aea78afcf9e888c41a7947538d163880fca2091c
Matt Austin <matt@m-austin.com>
2024-04-11 13:15:10 -0700

    A little cleaner code

--- policy/inspector/inspect.js
+++ policy/inspector/inspect.js
@@ -1,113 +1,85 @@
 // node --experimental-policy=policy.json --experimental-websocket inspect.js
 
-;(async () => {
-    process._debugProcess(process.pid)
-  
-    let data = await fetch('http://127.0.0.1:9229/json').then((resp) => {
-      return resp.json()
-    })
-    let wsURL = data[0].webSocketDebuggerUrl
-  
-    let chromDebugger = new SimpleChromDebugger()
-    await chromDebugger.connect(wsURL)
-  
-    chromDebugger.addEventListener('Debugger.scriptParsed', async (e) => {
-      //console.log(e.type, e.detail);
-      let data = e.detail
-  
-      if (data.url == 'node:internal/policy/manifest') {
-        let { scriptSource } = await chromDebugger.send(
-          'Debugger.getScriptSource',
-          {
-            scriptId: data.scriptId,
-          }
-        )
-        const lineNumber = scriptSource
-          .substring(0, scriptSource.indexOf('if (result === kFallThrough)'))
-          .split('\n').length
-        await chromDebugger.send('Debugger.setBreakpointByUrl', {
+// node --experimental-policy=policy.json --experimental-websocket inspect.js
+
+(async () => {
+  // Debug process
+  process._debugProcess(process.pid);
+
+  // Fetch data and extract WebSocket URL
+  const data = await fetch('http://127.0.0.1:9229/json')
+  .then((resp) => resp.json());
+  const wsURL = data[0].webSocketDebuggerUrl;
+
+  // Create and connect to Chrome Debugger
+  const chromDebugger = new SimpleChromDebugger();
+  await chromDebugger.connect(wsURL);
+
+  chromDebugger.addEventListener('Debugger.scriptParsed', async (e) => {
+    const { url, scriptId } = e.detail;
+    if (url == 'node:internal/policy/manifest') {
+      let { scriptSource } = await chromDebugger.send('Debugger.getScriptSource',{scriptId})
+      const lineNumber = scriptSource.split('\n').findIndex((line) => line.includes('if (result === kFallThrough)'));
+
+      await chromDebugger.send('Debugger.setBreakpointByUrl', {
+          url,
           lineNumber: lineNumber - 1,
-          url: data.url,
           columnNumber: 0,
           condition: '((result = kFallThrough),false)',
-        })
-      }
-    })
-  
-    await chromDebugger.send('Debugger.enable')
-  })()
-  
+        });
+    }
+  })
+
+  await chromDebugger.send('Debugger.enable')
+
   setTimeout(() => {
-    console.log(require('child_process'))
+      console.log(require('child_process'))
   }, 1000)
-  
-  class SimpleChromDebugger extends EventTarget {
-    constructor() {
-      super()
-      this.methodMap = new Map()
-      this.socket = undefined
-      this.id = 1
-    }
-  
-    async send(method, params) {
-      this.socket.send(
-        JSON.stringify({
-          id: this.id,
-          method,
-          params,
-        })
-      )
-  
-      const messagePromise = new Promise((resolve, reject) => {
-        this.methodMap.set(this.id, {
-          resolve,
-          reject,
-        })
-      })
-  
-      this.id += 1
-      return messagePromise
-    }
-  
-    async connect(url) {
-      this.socket = new WebSocket(url)
-  
-      return new Promise((resolve, reject) => {
-        this.socket.addEventListener('open', (event) => {
-          resolve(this)
-        })
-  
-        this.socket.addEventListener('error', (event) => {
-          reject(event)
-        })
-  
-        this.socket.addEventListener('message', (event) => {
-          let data = JSON.parse(event.data)
-  
-          this.dispatchEvent(
-            new CustomEvent('message', {
-              detail: data,
-            })
-          )
-  
-          if (data.method) {
-            this.dispatchEvent(
-              new CustomEvent(data.method, {
-                detail: data.params,
-              })
-            )
-          }
-  
-          if (data.id && this.methodMap.has(data.id)) {
-            const p = this.methodMap.get(data.id)
-            if (data.result) {
-              p.resolve(data.result)
-            } else if (data.error) {
-              p.reject(data.error)
-            }
-            this.methodMap.delete(data.id)
-          }
-        })
-      })
-    }
+
+})()
+
+
+
+class SimpleChromDebugger extends EventTarget {
+  constructor() {
+    super();
+    this.id = 1;
+    this.methodMap = new Map();
+    this.socket = undefined;
   }
+
+  async connect(url) {
+    this.socket = new WebSocket(url);
+
+    return new Promise((resolve, reject) => {
+      this.socket.addEventListener('open', () => resolve(this));
+      this.socket.addEventListener('error', reject);
+      this.socket.addEventListener('message', (event) => {
+        const data = JSON.parse(event.data);
+        this.dispatchEvent(new CustomEvent('message', { detail: data }));
+        if (data.method) {
+          this.dispatchEvent(new CustomEvent(data.method, { detail: data.params }));
+        }
+        if (data.id && this.methodMap.has(data.id)) {
+          const { resolve, reject } = this.methodMap.get(data.id);
+          if (data.result) {
+            resolve(data.result);
+          } else if (data.error) {
+            reject(data.error);
+          }
+          this.methodMap.delete(data.id);
+        }
+      });
+    });
+  }
+
+  async send(method, params) {
+    const messageId = this.id++;
+    this.methodMap.set(messageId, { resolve: null, reject: null });
+    this.socket.send(JSON.stringify({ id: messageId, method, params }));
+
+    return new Promise((resolve, reject) => {
+      this.methodMap.set(messageId, { resolve, reject });
+    });
+  }
+}
